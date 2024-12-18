e72503b6d83916e54b57acb44599824114aa3651
bubao <asd565586630@gmail.com>
2024-12-19 01:21:13 +0800

    add readme

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,6 @@
 const prisma = require("./db/prisma");
 const joiValidator = require("./joi/validate");
 
-
-require("dotenv")
-	.config();
-
 const errcode = require("./error/errcode");
 const MyError = require("./error/MyError");
 module.exports = {
